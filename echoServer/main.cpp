329533bf79f1125a64f77729772d6563f3e21685
youngjoo.park <whyj.park@gmail.com>
2021-03-19 17:50:29 +0900

    select 기반 server 로 변경 1차

--- echoServer/main.cpp
+++ echoServer/main.cpp
@@ -11,6 +11,10 @@ const short MAX_MSG_LEN = 256;
 std::vector<std::thread> workers;
 std::atomic<bool> bCOUT = false;
 
+fd_set read;
+fd_set temp;
+TIMEVAL timeV;
+
 SOCKET SetTCPServer(short portnum, int blog)
 {
 	SOCKET sock;
@@ -34,9 +38,12 @@ SOCKET SetTCPServer(short portnum, int blog)
 	if (re == SOCKET_ERROR) {
 		return - 1;
 	}
+
+	FD_ZERO(&read);
+	FD_SET(sock, &read);
 	return sock;
 }
-void DoWork(SOCKET dosock)
+void DoWorkLoop(SOCKET dosock)
 {
 	char msg[MAX_MSG_LEN] = "";
 	while (recv(dosock, msg, sizeof(msg), 0) > 0) {
@@ -49,9 +56,61 @@ void DoWork(SOCKET dosock)
 	}
 	closesocket(dosock);
 }
+void DoWork(SOCKET dosock)
+{
+	char msg[MAX_MSG_LEN] = "";
+	auto len = recv(dosock, msg, sizeof(msg), 0);
+	if(len != 0){
+		if (bCOUT == false) {
+			bCOUT.store(true);
+			printf("recv: %s \n", msg);
+			bCOUT.store(false);
+		}
+		send(dosock, msg, sizeof(msg), 0);
+	}
+	else {
+		closesocket(dosock);
+	}
+}
 
 void AcceptLoop(SOCKET sock) 
 {
+	int re;
+	while (1) {
+		temp = read;
+		timeV.tv_sec = 1;
+		timeV.tv_usec = 0;
+		re = select(NULL, &temp, NULL, NULL, &timeV);
+		if (re == SOCKET_ERROR) {
+			break;
+		}
+		if (re == 0) {
+			continue;
+		}
+		for (int i = 0; i < read.fd_count; ++i) {
+			if (FD_ISSET(read.fd_array[i], &temp)) {
+				if (sock == read.fd_array[i]) {
+					SOCKADDR_IN clientAddr = { 0, };
+					int len = sizeof(clientAddr);
+					SOCKET dosock = accept(sock, (SOCKADDR*)&clientAddr, &len);
+					if (dosock == SOCKET_ERROR) {
+						break;
+					}
+					FD_SET(dosock, &read);
+					if (bCOUT == false) {
+						bCOUT.store(true);
+						printf("%s: %d accept connect \n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+						bCOUT.store(false);
+					}
+				}
+				else {
+					DoWork(read.fd_array[i]);
+				}
+			}
+		}
+	}
+	return;
+
 	SOCKET dosock;
 	SOCKADDR_IN clientAddr = { 0, };
 	int len = sizeof(clientAddr);
