6d80b2a92723759f6fc26a4f404cc142de875ab3
youngjoo.park <whyj.park@gmail.com>
2021-03-22 11:33:44 +0900

    boost sample 기준 iocp -> boost::asio 로 서버 변경

--- echoServer/main.cpp
+++ echoServer/main.cpp
@@ -16,130 +16,121 @@ const short MAX_MSG_LEN = 256;
 std::vector<std::thread> workers;
 std::mutex mutexG;
 
-//void WorkerThread(HANDLE hIOCP)
-//{
-//	SOCKET dosock;
-//	DWORD receiveBytes;
-//	DWORD bytesSend;
-//
-//	while (1) {
-//		if (GetQueuedCompletionStatus(hIOCP, &receiveBytes, (PULONG_PTR)&keyInfo, (LPOVERLAPPED*)&info, INFINITE)) {
-//			dosock = keyInfo->socket;
-//			{
-//				std::lock_guard<std::mutex> lock_guard(mutexG);
-//				printf("%s: %d recv from queued [%d] \n",
-//					keyInfo->ipADDR.c_str(),
-//					keyInfo->port,
-//					(int)keyInfo->socket);
-//				if (receiveBytes == SOCKET_ERROR) {
-//					closesocket(dosock);
-//				}
-//				else if (receiveBytes != 0) {
-//					printf("recv: %s  [ num : %d ]\n", info->msg, info->myNum);
-//
-//					auto len = WSASend(dosock, (LPWSABUF)&info->dataBuffer, 1, &bytesSend, 0, NULL, NULL);
-//
-//					if (len == SOCKET_ERROR) {
-//						if (WSAGetLastError() != WSA_IO_PENDING) {
-//							closesocket(dosock);
-//							break;
-//						}
-//					}
-//					WSARecv(dosock, &info->dataBuffer, 1, &receiveBytes, &flags, &info->overlapped, NULL);
-//
-//					if (len == SOCKET_ERROR) {
-//						if (WSAGetLastError() != WSA_IO_PENDING) {
-//							closesocket(dosock);
-//							break;
-//						}
-//					}
-//				}
-//			}
-//		}
-//		else {
-//			closesocket(dosock);
-//			break;
-//		}
-//	}
-//}
-//SOCKET SetTCPServer(short portnum, int blog)
-//{
-//	SOCKET sock;
-//	sock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0,WSA_FLAG_OVERLAPPED);
-//	if (sock == INVALID_SOCKET) {
-//		return -1;
-//	}
-//	SOCKADDR_IN serverAddr = { 0, };
-//	serverAddr.sin_family = AF_INET;
-//	serverAddr.sin_addr = GetMyIP();
-//	serverAddr.sin_port = htons(portnum);
-//
-//	std::cout << inet_ntoa(serverAddr.sin_addr);
-//
-//	int re = 0;
-//	re = bind(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-//	if (re == SOCKET_ERROR) {
-//		errPrint("bind ERR");
-//		return -1;
-//	}
-//	re = listen(sock, blog);
-//	if (re == SOCKET_ERROR) {
-//		errPrint("listen ERR");
-//		return - 1;
-//	}
-//	return sock;
-//}
-//
-////iocp 에서는 accept 된 socket 을 iocp 와 연결해주며 async recv 를 호출해두어 대기상태로 돌린다
-//void AcceptLoop(SOCKET sock) 
-//{
-//	while (1) {
-//		SOCKADDR_IN clientAddr = { 0, };
-//		int len = sizeof(clientAddr);
-//		SOCKET dosock = accept(sock, (SOCKADDR*)&clientAddr, &len);
-//		if (dosock == INVALID_SOCKET) {
-//			break;
-//		} 
-//		{
-//			std::lock_guard<std::mutex> lock_guard(mutexG);
-//				static int nALL = 1;
-//				printf("%s: %d accept connect [%d in %d] \n",
-//					inet_ntoa(clientAddr.sin_addr),
-//					ntohs(clientAddr.sin_port),
-//					(int)dosock,
-//					nALL++);
-//
-//			info = new sInfo();
-//			info->socket = dosock;
-//			info->dataBuffer.buf = info->msg;
-//			info->dataBuffer.len = MAX_MSG_LEN;
-//			info->myNum = nALL - 1;
-//		}
-//
-//		keyInfo = new skeyInfo();
-//		keyInfo->socket = dosock;
-//		keyInfo->ipADDR = inet_ntoa(clientAddr.sin_addr);
-//		keyInfo->port = clientAddr.sin_port;
-//		keyInfo->clientAddr = clientAddr;
-//
-//		CreateIoCompletionPort((HANDLE)dosock, hIOCP, (DWORD)keyInfo, 0);
-//		auto ret = WSARecv(info->socket, &info->dataBuffer, 1, &receiveBytes, &flags, &info->overlapped, NULL);
-//		if (ret == SOCKET_ERROR) {
-//			if (WSAGetLastError() != WSA_IO_PENDING) {
-//				errPrint("firstWSARecv_ERROR");
-//				break;
-//			}
-//		}
-//	}
-//	closesocket(sock);
-//	return;
-//}
+using boost::asio::ip::tcp;
 
+void WorkerThread(tcp::socket& socket)
+{
+	SOCKET dosock;
+	DWORD receiveBytes;
+	DWORD bytesSend;
+
+	while (1) {
+		/*if (GetQueuedCompletionStatus(hIOCP, &receiveBytes, (PULONG_PTR)&keyInfo, (LPOVERLAPPED*)&info, INFINITE)) {
+			dosock = keyInfo->socket;
+			{
+				std::lock_guard<std::mutex> lock_guard(mutexG);
+				printf("%s: %d recv from queued [%d] \n",
+					keyInfo->ipADDR.c_str(),
+					keyInfo->port,
+					(int)keyInfo->socket);
+				if (receiveBytes == SOCKET_ERROR) {
+					closesocket(dosock);
+				}
+				else if (receiveBytes != 0) {
+					printf("recv: %s  [ num : %d ]\n", info->msg, info->myNum);
+
+					auto len = WSASend(dosock, (LPWSABUF)&info->dataBuffer, 1, &bytesSend, 0, NULL, NULL);
+
+					if (len == SOCKET_ERROR) {
+						if (WSAGetLastError() != WSA_IO_PENDING) {
+							closesocket(dosock);
+							break;
+						}
+					}
+					WSARecv(dosock, &info->dataBuffer, 1, &receiveBytes, &flags, &info->overlapped, NULL);
+
+					if (len == SOCKET_ERROR) {
+						if (WSAGetLastError() != WSA_IO_PENDING) {
+							closesocket(dosock);
+							break;
+						}
+					}
+				}
+			}
+		}
+		else {
+			closesocket(dosock);
+			break;
+		}*/
+	}
+}
+class session : public std::enable_shared_from_this<session>
+{
+public:
+	session(tcp::socket socket) : socket_(std::move(socket))
+	{
+
+	}
+	void start()
+	{
+		do_read();
+	}
+private:
+	void do_read() {
+		auto self(shared_from_this());
+		socket_.async_read_some(boost::asio::buffer(data_, MAX_MSG_LEN),
+			[this, self](boost::system::error_code ec, std::size_t length)
+			{
+				if (!ec) {
+					{
+						std::lock_guard<std::mutex> lock_guard(mutexG); 
+						printf("recv: %s \n", data_);
+					}
+					do_write(length);
+				}
+			}
+		);
+	}
+	void do_write(std::size_t length) {
+		auto self(shared_from_this());
+		boost::asio::async_write(socket_, boost::asio::buffer(data_, MAX_MSG_LEN),
+			[this, self](boost::system::error_code ec, std::size_t /*length*/)
+			{
+				if (!ec) {
+					do_read();
+				}
+			}
+		);
+	}
+	tcp::socket socket_;
+	char data_[MAX_MSG_LEN];
+};
+class server
+{
+public:
+	server(boost::asio::io_service& io_service, short port) : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)), socket_(io_service)
+	{
+		do_accept();
+	}
+private:
+	void do_accept()
+	{
+		acceptor_.async_accept(socket_,
+			[this](boost::system::error_code ec) {
+				if (!ec) {
+					std::make_shared<session>(std::move(socket_))->start();
+				}
+				do_accept();
+			});
+	}
+	tcp::acceptor acceptor_;
+	tcp::socket socket_;
+};
 int main()
 {
-	//SOCKET sock = SetTCPServer(PORT_NUM, BLOG_SIZE);
-	//if (sock != SOCKET_ERROR) {
-		//AcceptLoop(sock);		
-	//}
+	boost::asio::io_service io_service_S;
+	server s(io_service_S, PORT_NUM);
+	io_service_S.run();
+
 	return 0;
 }
